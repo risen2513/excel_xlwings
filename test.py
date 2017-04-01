42cc6d98cfccdb7b1e2740833591f0638aa1d6a6
Risen <risen2513@gmail.com>
2017-04-01 20:52:55 +0800

    通过配置文件设置指定列

--- test.py
+++ test.py
@@ -1,22 +1,22 @@
-# -*- coding: utf-8 -*-
-
-
-
-
-# if __name__ == '__main__':
-    # find_cols = []
-    set_cols = []#[[] for i in range(2)]
-    file = open("./testdata/copy_setting.txt")
-    i = 1
-    for line in file:
-        line = line.strip()
-        # print i, ":", line
-        if i == 1:
-            find_cols = line.split(',')
-        if i == 2:
-            arr = line.split('#')
-            set_cols.append(arr[0])
-            set_cols.append(arr[1].split(','))
-        i += 1
-    print find_cols
-    print set_cols
+# # -*- coding: utf-8 -*-
+#
+#
+#
+#
+# # if __name__ == '__main__':
+#     # find_cols = []
+#     set_cols = []#[[] for i in range(2)]
+#     file = open("./testdata/copy_setting.txt")
+#     i = 1
+#     for line in file:
+#         line = line.strip()
+#         # print i, ":", line
+#         if i == 1:
+#             find_cols = line.split(',')
+#         if i == 2:
+#             arr = line.split('#')
+#             set_cols.append(arr[0])
+#             set_cols.append(arr[1].split(','))
+#         i += 1
+#     print find_cols
+#     print set_cols
